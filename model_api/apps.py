f544b6eb0fd6f0bb6aefc1d36dcb928d642b9852
alexey_ignatov <alexey.ignatov.msu@gmail.com>
2020-07-06 10:58:55 +0300

    many options

--- model_api/apps.py
+++ model_api/apps.py
@@ -21,10 +21,13 @@ class ModelApiConfig(AppConfig):
         tmp_config['chainer']['out'] = ['y_pred_labels', 'y_pred_probas']
         tmp_config['chainer']['pipe'][-2]['out'] = ['y_pred_ids', 'y_pred_probas']
 
+        global in_2_label
+        in_2_label = pickle.load(open('./../any_tc_bot/in_2_label.pkl', 'rb'))
+
 
         global intent_model
         intent_model = build_model(tmp_config)
+
         print('модель загрузили кое-как')
         print("ok!")
-
 
--- model_api/views.py
+++ model_api/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 # Create your views here.
-from model_api.apps import intent_model
+from model_api.apps import intent_model,in_2_label
+import pandas as pd
 
+def predict(name,model,in_2_label, top=100000):
+    res_l, probs = model([name.lower()])
+    res_dict = {}
+    for k, v in in_2_label.items():
+        res_dict[k] = probs[0][v]
+
+    return res_dict
 
 class IntentModelApi(APIView):
     @staticmethod
     def get(request):
-        return Response({"intent_type": intent_model([str(request.data['context']).lower()])[0]})
+
+        res_dict = predict(str(request.data['context']).lower(),intent_model,in_2_label)
+
+        #return Response({"intent_type":pd.Series(res_dict).sort_values().index[-1]})
+        return Response({"predict_dict": res_dict})
