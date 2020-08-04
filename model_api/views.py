8a964180c69942fba296b0a320aec68dfbcdcac1
Alexey Ignatov <alexey.ignatov.msu@gmail.com>
2020-08-04 04:44:30 +0300

    new var (no ukn)

--- model_api/views.py
+++ model_api/views.py
@@ -5,19 +5,28 @@ from rest_framework.response import Response
 from model_api.apps import intent_model,in_2_label
 import pandas as pd
 
-def predict(name,model,in_2_label, top=100000):
+
+
+def predict(name,model,in_2_label):
+    th1 = .8
+    th2 = .15
     res_l, probs = model([name.lower()])
     res_dict = {}
     for k, v in in_2_label.items():
         res_dict[k] = probs[0][v]
-
-    return res_dict
+    most_rel_intents_ser = pd.Series(res_dict).sort_values()[-3:]
+    print(most_rel_intents_ser)
+    if most_rel_intents_ser.sum() < th1:
+        return ['ukn']
+    if (most_rel_intents_ser > th2).any():
+        return most_rel_intents_ser[most_rel_intents_ser > th2].index.tolist()
+    return ['ukn']
 
 class IntentModelApi(APIView):
     @staticmethod
     def get(request):
 
-        res_dict = predict(str(request.data['context']).lower(),intent_model,in_2_label)
-        print(res_dict)
+        intent_list = predict(str(request.data['context']).lower(),intent_model,in_2_label)
+        print(intent_list)
         #return Response({"intent_type":pd.Series(res_dict).sort_values().index[-1]})
-        return Response({"predict_dict": res_dict})
+        return Response({"intent_list": intent_list})
