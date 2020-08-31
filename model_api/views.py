cc6de19a892d576a6cabffc3e0a47b5218b4d294
Alexey Ignatov <alexey.ignatov.msu@gmail.com>
2020-08-31 13:27:19 +0300

    предсказание на стороне клиента

--- model_api/views.py
+++ model_api/views.py
@@ -6,27 +6,14 @@ from model_api.apps import intent_model,in_2_label
 import pandas as pd
 
 
-
-def predict(name,model,in_2_label):
-    th1 = .9
-    th2 = .15
-    res_l, probs = model([name.lower()])
-    res_dict = {}
-    for k, v in in_2_label.items():
-        res_dict[k] = probs[0][v]
-    most_rel_intents_ser = pd.Series(res_dict).sort_values()[-3:]
-    print(most_rel_intents_ser)
-    if most_rel_intents_ser.sum() < th1:
-        return ['ukn']
-    if (most_rel_intents_ser > th2).any():
-        return most_rel_intents_ser[most_rel_intents_ser > th2].index.tolist()
-    return ['ukn']
-
 class IntentModelApi(APIView):
     @staticmethod
     def get(request):
+        # intent_list = predict(str(request.data['context']).lower(),intent_model,in_2_label)
+        res_l, probs = intent_model([str(request.data['context']).lower()])
+        res_dict = {}
+        for k, v in in_2_label.items():
+            res_dict[k] = probs[0][v]
 
-        intent_list = predict(str(request.data['context']).lower(),intent_model,in_2_label)
-        print(intent_list)
-        #return Response({"intent_type":pd.Series(res_dict).sort_values().index[-1]})
-        return Response({"intent_list": intent_list})
+        # return Response({"intent_type":pd.Series(res_dict).sort_values().index[-1]})
+        return Response({"intent_list": res_dict})
